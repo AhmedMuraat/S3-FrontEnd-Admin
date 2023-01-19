ab15df3c366bc6c4a8de93a08b7ec248d9398e13
Muraat,ahmed A <a.muraat@student.fontys.nl>
2023-01-19 18:45:04 +0100

    Variables

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 export function Home() {
-
+console.log(localStorage.getItem("Token"));
   return (
     <div data-testid="HomeTest">
       <h1 className="text-center">Welcome admin</h1>
